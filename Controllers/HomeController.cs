869dae3b4046608b050513e51409606f9faf11f2
Toba Shaukat <toba.thetasolutions@outlook.com>
2021-10-29 16:41:00 +0500

    Home Controller > my first page action added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace MyFirstPractice.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult FirstPage()
+        {
+            return View();
+        }
     }
 }
